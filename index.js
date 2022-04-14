32a5c8521e2af4647b94b943dc8f920ac72e8a27
Lovepreet Singh <lovesingh@bebotechnologies.com>
2022-04-14 16:12:08 +0530

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser =  require("body-parser");
 const app =  express();
-const PORT = 8080;
+const PORT = 3000;
 app.use(bodyParser.json());
 app.use("/",express.static(path.join(__dirname, "public")));
 
